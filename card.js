f3b1aa571b4cf83244db7740e6f70015b5383d3c
pratiksha950 <salunkepratiksha950@gmail.com>
2025-09-08 22:15:16 +0530

    card completed

--- card.js
+++ card.js
@@ -1,7 +1,15 @@
-const foodItems=["vada pav","mango","dosa","dhokala"];
+const foodItems=["Vada Pav","Mango Ice Cream","Dosa","Dhokala","Idali"];
 const foodList=document.querySelector("#list");
-
-for(let     i=0;i<foodItems.length;i++){
+for(let i=0;i<foodItems.length;i++){
     foodList.innerHTML+=`<div class="foodCard">
-    <h2>${foodItems[i]}</h2></div>  `
+    <h2>${foodItems[i]}${randomImoji()}</h2></div>  `
 }
+function randomImoji(){
+    const Emoji=["🥰","😊","😋","😎","😍","☺️","🙂","🤗","🤓"];
+    const min=0;
+    const max=Emoji.length;
+    const EmojiIndex=Math.floor(Math.random()*(max-min)+min);
+    return Emoji[EmojiIndex];
+}
+const emoji=randomImoji();
+console.log(emoji);
